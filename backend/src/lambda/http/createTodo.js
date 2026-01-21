c7ad606c063c94db4c4be66644bafa1f9830424b
Archana S <110758371+ArchanaSenthilvel@users.noreply.github.com>
2026-01-21 23:34:09 +0530

    Add files via upload

---
+++ backend/src/lambda/http/createTodo.js
@@ -0,0 +1,47 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
+import { createTodo } from '../../businessLogic/todos'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    // TODO: Implement creating a new TODO item
+    try{
+      const userId = getUserId(event)
+      const newItem = await createTodo(newTodo, userId)
+
+      return {
+        statusCode: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          item: newItem
+        })
+      }
+    } catch (error) {
+      console.error('Error fetching todos:', error)
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          error: 'Failed to fetch todos'
+        })
+      }
+    }  
+    }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
---
+++ backend/src/lambda/http/generateUploadUrl.js
@@ -0,0 +1,50 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+    try{
+      const userId = getUserId(event)
+      const uploadUrl = await createAttachmentPresignedUrl(userId, todoId)
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          uploadUrl
+        })
+      }
+    } catch (error) {
+      console.error('Error fetching todos:', error)
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          error: 'Failed to fetch todos'
+        })
+      }
+    }  
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
