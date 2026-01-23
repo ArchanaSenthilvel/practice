c39f75c5c060a02c75464458bf7a4a79ecb8ec27
Archana S <110758371+ArchanaSenthilvel@users.noreply.github.com>
2026-01-23 23:19:26 +0530

    Update attachmentUtils.js

--- backend/src/helpers/attachmentUtils.js
+++ backend/src/helpers/attachmentUtils.js
@@ -1,20 +1,20 @@
-import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
-
+import AWS from 'aws-sdk'
+import AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
-
 export class AttachmentUtils {
   constructor(
-    private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
-    private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
-    private readonly urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION || '300')
-  ) {}
-
-  getAttachmentUrl(todoId: string): string {
+    s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+    bucketName = process.env.ATTACHMENT_S3_BUCKET,
+    urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+  ) {
+    this.s3 = s3
+    this.bucketName = bucketName
+    this.urlExpiration = urlExpiration
+  }
+  getAttachmentUrl(todoId) {
     return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
   }
-
-  getUploadUrl(todoId: string): string {
+  getUploadUrl(todoId) {
     return this.s3.getSignedUrl('putObject', {
       Bucket: this.bucketName,
       Key: todoId,
